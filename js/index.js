09a44eb3b541dc78889a1e81366d8c0b51aaa7fd
lvjian1212 <928808423@qq.com>
2019-03-25 17:58:51 +0800

    jquery

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// // {
+// //     x = Math.PI;
+// //     cx = Math.cos(x);
+// //     console.log('cos(1)='+cx)
+// // }
+
+// // console.log(s)
+// // console.log(d);
+
+// // var s = function(){}
+// // function d(){}
+
+// // if(!s)
+// // console.log(1)
+// // else;
+
+
+// var o = {
+//     next:{
+//         next:{
+//             next:{
+//                 next:{
+//                     next:{p:222}
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// function Ofn(o){
+// for(;o.next;o=o.next)/*empty */;
+// return o
+// }
+// console.log(Ofn(o))
+
+
+if(true){
+    throw new Error('212122')
+}
